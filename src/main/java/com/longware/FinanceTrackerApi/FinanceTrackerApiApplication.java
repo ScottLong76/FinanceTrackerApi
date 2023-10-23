b6f670a9ffc964cfd0f11c4c51b2dad650d2c518
Scott Long <ScottLong76@gmail.com>
2023-10-23 00:13:00 -0400

    Initial Spring Boot checkin

---
+++ src/main/java/com/longware/FinanceTrackerApi/FinanceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.longware.FinanceTrackerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApiApplication.class, args);
+	}
+
+}
